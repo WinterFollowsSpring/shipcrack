3d000e1f4ac9db685d557da7b8a91f6253719829
WinterFollowsSpring <winterfollowsspring@gmail.com>
2022-06-12 18:43:13 -0400

    Testing rendering a template

--- shipcrack.py
+++ shipcrack.py
@@ -1,15 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello_world():
-    return 'Hello from ShipCrack!'
-
-@app.route('/ships')
-def ships():
-    return '<head><title>ShipCrack - Ships</title></head><body>This is the Ships page! You\'ll search for ships here</body>'
-
+    return render_template('templates/search.html')
