361e07edb2d45d1b469bba51f9b6c0221ba8890b
WinterFollowsSpring <winterfollowsspring@gmail.com>
2022-06-12 20:37:31 -0400

    Testing jinja templates

--- shipcrack.py
+++ shipcrack.py
@@ -1,11 +1,17 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def hello_world():
+comments = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index_page():
+    if request.method = 'POST':
+        comments.append(request.form('contents'))
+        return redirect(url_for('index'))
+
     return render_template('ships.html')
